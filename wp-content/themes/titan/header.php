0bfbdb26d5e06e98bb76c1951056e6d8b70b6b5c
Ross Bruniges <rossbruniges@gmail.com>
2012-02-20 08:48:29 +0000

    The less hardcoded stuff the better

--- wp-content/themes/titan/header.php
+++ wp-content/themes/titan/header.php
@@ -47,19 +47,6 @@
 	<!--WP Hook-->
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 	<?php wp_head(); ?>
-	<script type="text/javascript">
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-18758196-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-
-  </script>
 </head>
 <body <?php body_class(); ?>>
 	<div class="skip-content"><a href="#content">Skip to content</a></div>
