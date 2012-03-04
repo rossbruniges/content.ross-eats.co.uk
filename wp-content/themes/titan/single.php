9c6aaa5cd5f939c5bd04f2f1347d4807aecd7dbd
Ross Bruniges <rossbruniges@gmail.com>
2012-03-04 17:44:11 +0000

    Removing bad old profile, which likely contains dodgey stuff

--- wp-content/themes/titan/single.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if (in_category('Reviews')) {
-    include 'single-review.php';
-  } elseif (in_category('London Burritos')) {
-      include 'single-burrito.php';
-  } else {
-    include 'single-standard.php';
-  }
-?>
