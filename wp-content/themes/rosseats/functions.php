dffeb7f6ee94f55f55f658e9923ecb0f6a5dd44f
Ross Bruniges <rossbruniges@gmail.com>
2013-03-11 07:39:38 +0000

    Adding in a more robust feature check for the GA snippet embed

--- wp-content/themes/rosseats/functions.php
+++ wp-content/themes/rosseats/functions.php
@@ -42,3 +42,27 @@ function getImageAttachmentData( $post_id, $size = 'thumbnail', $count = 1 ) {
         return ( count( $attachments ) == 1 ) ? $meta : $objMeta;
     }
 }
+
+function add_analytics () {
+	
+	if (!current_user_can('manage_options')) {
+	    if (defined('GA_CODE')) {
+		echo "<script type='text/javascript'>
+  			var _gaq = _gaq || [];
+  			_gaq.push(['_setAccount', '" . GA_CODE . "']);
+  			_gaq.push(['_trackPageview']);
+  			(function() {
+    			var ga = document.createElement('script'); ga.type = 'text/javascript';
+				ga.async = true;
+   	 			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    			var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(ga, s);
+  			})();
+			</script>";
+		}
+	}
+}
+
+add_action('wp_head', 'add_analytics', 100);
+
+?>
