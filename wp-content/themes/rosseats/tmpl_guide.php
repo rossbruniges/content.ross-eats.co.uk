b1d44e2b49cf6b7de6198b41d4026776d2714261
Ross Bruniges <rossbruniges@gmail.com>
2013-07-04 09:45:54 +0100

    Fix bug #3 - reviews on guide page ordered by rated and inside of that date descending

--- wp-content/themes/rosseats/tmpl_guide.php
+++ wp-content/themes/rosseats/tmpl_guide.php
@@ -9,30 +9,29 @@ then displays them in a lovely ordered list
 <?php get_header(); ?>
 	<?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-		<h1 class="pagetitle"><?php the_title(); ?></h1>
+		<h1 class="pagetitle">Ross eats in <?php the_title(); ?></h1>
 		<div class="entry page clear">
 			<?php the_content(); ?>
 			<?php
                 $page_slug = $post->post_name;
             ?>
-			<ol class="review_list">
-			<?php
-			    // create a wordpress loop that will be used to hold reviews
-			    $review_list = new WP_Query('category_name=reviews&posts_per_page=-1&meta_key=Restaurant rating&orderby=meta_value_num&order=DESC&tag=' . $page_slug);
-			    $current_rating = 0;
-			    while ($review_list->have_posts()) : $review_list->the_post();
-			        $review_details = get_post_custom($post->ID);
-			?>
-			    <?php
-			        if ($current_rating != $review_details['Restaurant rating'][0]) {
-			            if ($current_rating != 0) {
-			                echo "</ul></li>";
-			            }
-			            echo "<li>";
-			            echo "<h2>Rated " . $review_details['Restaurant rating'][0] . " <span>(out of 10)</span></h2><ul>";
-			        } 
-			    ?>
-			        <li class="vcard">
+            <ol class="review_list">
+            	<?php
+            		for ($i=10; $i > 0; $i--) {
+            			$rated_list = new WP_Query(array(
+			    			'category_name' => 'reviews',
+			    			'posts_per_page' => -1,
+			    			'orderby' => 'date',
+			    			'order' => 'DESC',
+			    			'meta_key' => 'Restaurant rating',
+			    			'meta_value' => $i,
+			    			'tag' => $page_slug));
+            			if ($rated_list->have_posts()) {
+            				echo '<li><h2>Rated ' . $i . ' <span>(out of 10)</span></h2><ul>';
+            				while ($rated_list->have_posts()) : $rated_list->the_post();
+			        			$review_details = get_post_custom($post->ID);
+			        	?>
+			        		<li class="vcard">
 			        <h3 class="fn"><?php echo str_replace('reviewed', '', get_the_title()); ?></h3>
 			        <div class="summary">
 			            
@@ -51,11 +50,12 @@ then displays them in a lovely ordered list
                         <?php endif; ?>
                         
 			        </li>
-			    <?php
-			        
-			        $current_rating = $review_details['Restaurant rating'][0];
-			    ?>
-            <?php endwhile; ?>
+			        	<?php endwhile; ?>
+			        		</ul></lu>
+			        	<?php
+			        	}
+            		}
+            	?>
             </ol>
             <?php wp_reset_postdata(); ?>
 			<?php edit_post_link(__( 'Edit', 'titan')); ?>
