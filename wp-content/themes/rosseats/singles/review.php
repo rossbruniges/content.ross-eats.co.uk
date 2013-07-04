1330a5c0b5da1dc8d6e00b4cdd245020f4ebad12
Ross Bruniges <rossbruniges@gmail.com>
2013-07-04 16:04:34 +0100

    Fix bug #4 - adding a link to the review lists opposed to related posts, which never really worked all that well and removed the yarpp plugin, which we no longer need

--- wp-content/themes/rosseats/singles/review.php
+++ wp-content/themes/rosseats/singles/review.php
@@ -62,11 +62,26 @@
  			<a href="https://twitter.com/share" class="twitter-share-button" data-text="Thought this was good enough to share" data-via="ross_eats">Tweet</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
  		</div>
- 		<?php if(function_exists('related_posts')): ?>
-            <div class="related_posts">
-                <?php related_posts(); ?>
+        <div class="related_posts">
+            <?php
+            $areas = array('london', 'america', 'uk');
+            $active_areas = array();
+            foreach (get_the_tags() as $tag) {
+                if (in_array($tag->name, $areas)) {
+                    array_push($active_areas, $tag->name);
+                }
+            }
+            if (count($active_areas) > 0) {
+                echo "<h2>Anything else 'near-by'?</h2>";
+                echo "<p>Looking for something a bit differet? See if I've got any other reviews from near-by (obviously the accuacy of 'near-by' could vary...)</p>";
+                echo "<ol>";
+                foreach ($active_areas as $value) {
+                    echo "<li><a href=/in/" . $value . ">" . $value . "</a></li>";
+                };
+                echo "</ol>";
+            }
+        ?>
             </div>
-        <? endif; ?>
         <div class="tags"><?php the_tags( '<span>Tags</span> <p>', ', ', '</p>'); ?></div>
  		<?php wp_link_pages(); ?>
  	</div><!--end entry-->
